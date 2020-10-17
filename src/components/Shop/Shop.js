99d387ca5282363be304e6d0a5826c373d95a214
Ifat-Fahim <ifatfahim007@gmail.com>
2020-10-17 13:04:52 +0600

    done shop component and product handler

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,18 +2,25 @@ import React, { useState } from 'react';
 import fakeData from './../../fakeData';
 import Product from './../Product/Product';
 import './Shop.css';
+import Cart from './../Cart/Cart';
 
 const Shop = () => {
     const [products, setProducts] = useState(fakeData);
+    const [cart, setCart] = useState([]);
+    function handleAddProduct(product) {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
                 {
-                    products.map(product => <Product key={product.key} product={product} />)
+                    products.map(product => <Product key={product.key} product={product}
+                        handleAddProduct={handleAddProduct} />)
                 }
             </div>
             <div className="cart-container">
-                <h1>This is cart container.</h1>
+                <Cart cart={cart} />
             </div>
         </div>
             
